6997962b14d90f69fc8e4fc84111ccf801b3fd4d
pwarren <pwarren@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-06-16 22:59:11 +0000

    Convert Text->TextLine / TextLine->Text for all schema types not modified since revision 41541. Reviewed by jgartman.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.ApacheMonitor@44253 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/ApacheMonitor/interfaces.py
+++ ZenPacks/zenoss/ApacheMonitor/interfaces.py
@@ -16,15 +16,15 @@ from Products.Zuul.utils import ZuulMessageFactory as _t
 
 
 class IApacheMonitorDataSourceInfo(IBasicDataSourceInfo):
-    component = schema.Text(title=_t(u'Component'))
-    eventKey = schema.Text(title=_t(u'Event Key'))
+    component = schema.TextLine(title=_t(u'Component'))
+    eventKey = schema.TextLine(title=_t(u'Event Key'))
     timeout = schema.Int(title=_t(u'Timeout (seconds)'))
-    hostname = schema.Text(title=_t(u'Apache Host'))
+    hostname = schema.TextLine(title=_t(u'Apache Host'))
     usessh = schema.Bool(title=_t(u'Use SSH'))
     port = schema.Int(title=_t(u'Apache Port'))
     ssl = schema.Bool(title=_t(u'Use HTTPS?'))
-    url = schema.Text(title=_t(u'Status URL'))
-    ngerror = schema.Text(title=_t(u'Named Group Regex Error'))
-    ngregex = schema.TextLine(title=_t(u'Named Group Regex'), xtype='twocolumntextarea')
+    url = schema.TextLine(title=_t(u'Status URL'))
+    ngerror = schema.TextLine(title=_t(u'Named Group Regex Error'))
+    ngregex = schema.Text(title=_t(u'Named Group Regex'), xtype='twocolumntextarea')
     
 
