bbce476006e781625befff2e736da7ca7c726c7c
gstreiff <gstreiff@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-07-24 05:16:47 +0000

    [trunk/core] update source headers per legal for 4.2.x and beyond using Summer's script fu; reviewed by gstreiff jhanson
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.ApacheMonitor@61801 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/ApacheMonitor/interfaces.py
+++ ZenPacks/zenoss/ApacheMonitor/interfaces.py
@@ -1,15 +1,13 @@
-###########################################################################
-#
-# This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2010, Zenoss Inc.
-#
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2 or (at your
-# option) any later version as published by the Free Software Foundation.
-#
-# For complete information please visit: http://www.zenoss.com/oss/
-#
-###########################################################################
+##############################################################################
+# 
+# Copyright (C) Zenoss, Inc. 2010, all rights reserved.
+# 
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+# 
+##############################################################################
+
+
 from Products.Zuul.interfaces import IBasicDataSourceInfo
 from Products.Zuul.form import schema
 from Products.Zuul.utils import ZuulMessageFactory as _t
@@ -26,5 +24,3 @@ class IApacheMonitorDataSourceInfo(IBasicDataSourceInfo):
     url = schema.TextLine(title=_t(u'Status URL'))
     ngerror = schema.TextLine(title=_t(u'Named Group Regex Error'))
     ngregex = schema.Text(title=_t(u'Named Group Regex'), xtype='twocolumntextarea')
-    
-
